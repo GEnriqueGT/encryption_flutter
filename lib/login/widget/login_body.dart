dc1c1775c26738cf78d128b010eda2de39a77d02
gabriel <ghurtarte@nabenik.dev>
2023-09-12 02:15:01 -0600

    base app configurada

---
+++ lib/login/widget/login_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginBody extends StatefulWidget {
+  const LoginBody({Key? key}) : super(key: key);
+
+  @override
+  State<LoginBody> createState() => _LoginBodyState();
+}
+
+class _LoginBodyState extends State<LoginBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/routes/landing_routes.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:password_manager/login/login_screen.dart';
+import 'package:password_manager/routes/landing_routes_constants.dart';
+
+class LandingRoutes {
+  static Route<dynamic> generateRouteLanding(final RouteSettings settings) {
+    switch (settings.name) {
+      case rootRoute:
+        return MaterialPageRoute(
+          builder: (_) => const LoginScreen(),
+        );
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(child: Text('No route defined for ${settings.name}')),
+          ),
+        );
+    }
+  }
+}
