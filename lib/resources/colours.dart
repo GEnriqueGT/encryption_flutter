818cac40ad3b0ee1072fb4ada6370822a78587b6
gabriel <ghurtarte@nabenik.dev>
2023-09-12 02:43:48 -0600

    creación constante de colores

---
+++ lib/resources/colours.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color darkPink = Color(0xFFC53364);
+const Color redPink = Color(0xFFFF7678);
+const Color pink = Color(0xFFFFC5C6);
+const Color purple = Color(0xFF372E52);
+const Color white = Color(0xFFFFFFFF);
